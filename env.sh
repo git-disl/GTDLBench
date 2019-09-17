6d442865e9a46c2bacbcbdf11583fbc2086eda32
Yanzhao Wu <yanzhao@node-0.yanzhao-qv57922.c4420-6422.emulab.net>
2019-09-17 12:17:30 -0600

    fix tensorflow install bug

--- env.sh
+++ env.sh
@@ -1,12 +1,16 @@
 #!bin/bash
 # set the root directory of GTDLBench.
-DLBENCH_ROOT=${DLBENCH_ROOT:=`pwd`}
+export DLBENCH_ROOT=${DLBENCH_ROOT:=`pwd`}
 
 # Update .profile for Miniconda
 BASHCONFIGFILE=~/.bashrc
 if [ -f "$BASHCONFIGFILE" ]; then
     source $BASHCONFIGFILE
 fi
+
+#if [ -x "$(command -v conda)" ]; then
+#    export CONDA_BASE=$(conda info --base)
+#fi
 
 # For Caffe
 export PATH=$PATH:$DLBENCH_ROOT/caffe/build/tools
--- tensorflow-install.sh
+++ tensorflow-install.sh
@@ -2,7 +2,13 @@
 source env.sh
 DLBENCH_ROOT=${DLBENCH_ROOT:=$HOME/GTDLBench}
 cd $DLBENCH_ROOT
+if ! [ -x "$(command -v conda)" ]; then
+    echo "Error: Please intall conda by executing bash pre-install.sh"
+    exit 1 
+fi
+source $(conda info --base)/etc/profile.d/conda.sh
 conda create -n tensorflow python=2.7
+source env.sh
 conda activate tensorflow
 # Two options are recommended to install TensorFlow
 # 1. Install via conda
