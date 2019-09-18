1a708081b11b6facd08373a8a8cfbb3aba1b19da
Yanzhao Wu <yanzhao@node-0.yanzhao-qv57922.c4420-6422.emulab.net>
2019-09-17 21:43:41 -0600

    update data path

--- models/cifar10/tensorflow/cifar10.py
+++ models/cifar10/tensorflow/cifar10.py
@@ -50,7 +50,7 @@ FLAGS = tf.app.flags.FLAGS
 # Basic model parameters.
 tf.app.flags.DEFINE_integer('batch_size', 128,
                             """Number of images to process in a batch.""")
-tf.app.flags.DEFINE_string('data_dir', '/home/yanzhaowu/data/cifar10',
+tf.app.flags.DEFINE_string('data_dir', '../../../data/cifar10',
                            """Path to the CIFAR-10 data directory.""")
 tf.app.flags.DEFINE_boolean('use_fp16', False,
                             """Train the model using fp16.""")
