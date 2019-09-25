bc70dfbf39fe8dc7793f44f18588b6d828caec34
Yanzhao Wu <yanzhaowumail@gmail.com>
2019-09-25 12:02:09 -0400

    object detection init

---
+++ data/coco/download_coco.sh
@@ -0,0 +1,4 @@
+wget http://images.cocodataset.org/zips/train2017.zip
+wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
+wget http://images.cocodataset.org/zips/test2017.zip
+wget http://images.cocodataset.org/annotations/image_info_test2017.zip 
---
+++ data/voc/download_voc.sh
@@ -0,0 +1,3 @@
+wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar
+wget http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
+wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtest_06-Nov-2007.tar
