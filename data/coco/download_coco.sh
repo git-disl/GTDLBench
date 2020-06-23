89d9f3a07f94ff69cea0ff229a368f770120cf89
Yanzhao Wu <yanzhaowumail@gmail.com>
2020-06-22 21:48:16 -0400

    add coco dataset

--- data/coco/download_coco.sh
+++ data/coco/download_coco.sh
@@ -2,3 +2,4 @@ wget http://images.cocodataset.org/zips/train2017.zip
 wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
 wget http://images.cocodataset.org/zips/test2017.zip
 wget http://images.cocodataset.org/annotations/image_info_test2017.zip 
+wget http://images.cocodataset.org/zips/val2017.zip
